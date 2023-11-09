03bd4f145bcc35e34623699c0c800dc4442da348
breiner <bhernandez790@soyudemedellin.edu.co>
2023-11-08 23:15:01 -0500

    gggh.pi

--- 1144.py
+++ 1144.py
@@ -5,6 +5,7 @@ usuarios = []
 concierto = "Los inquietos del vallenato"
 root = tk.Tk()
 root.title("Los inquietos del vallenato") 
+#REQUISITO DE  crear usuario"
 class Usuario:
     def __init__(self, nombre, apellido, edad, tipo):
         self.nombre = nombre
@@ -15,18 +16,18 @@ class Usuario:
 
     def __str__(self):
         return f"{self.nombre} {self.apellido} ({self.tipo}) - Edad: {self.edad}"
-
+#Crear reserva#
     def crear_reserva(self, concierto, cantidad_boletos):
         reserva = {"concierto": concierto, "cantidad_boletos": cantidad_boletos}
         self.reservas.append(reserva)
         print(f"Reserva creada para el concierto {concierto} - {cantidad_boletos} boletos")
-
+#Generar factura#
     def generar_factura(self):
         factura = {"usuario": str(self), "reservas": self.reservas}
         with open(f"{self.nombre}_{self.apellido}_factura.json", "w") as file:
             json.dump(factura, file)
         print("Factura generada y almacenada.")
-
+#Crear bar#
 class Bar:
     def __init__(self):
         self.bebidas = {
@@ -47,7 +48,7 @@ class Bar:
         }
         self.boletos_vip_disponibles = 40
         self.palco_disponible = True
-
+#Mostrar bebidas
     def mostrar_bebidas(self, tipo_usuario):
         print("Bebidas disponibles:")
         for categoria in self.bebidas:
@@ -69,8 +70,8 @@ class Bar:
                     print(f"{cantidad} {bebida} seleccionada(s) - Total: ${total:.2f}")
                     return
         print(f"{bebida} no está en el menú")
-
-# Funciones para la interfaz gráfica con Tkinter
+        #INTERFAZ DE REQUISITOS"
+#Crear Perfil(parte de interfaz) #
 def crear_perfil():
     perfil_window = tk.Toplevel(root)
     perfil_window.title("Crear Perfil")
@@ -94,7 +95,7 @@ def crear_perfil():
     label_tipo.grid(row=3, column=0, padx=10, pady=5)
     entry_tipo = tk.Entry(perfil_window)
     entry_tipo.grid(row=3, column=1, padx=10, pady=5)
-
+#Guardar perfil#
     def guardar_perfil():
         nombre = entry_nombre.get()
         apellido = entry_apellido.get()
@@ -112,7 +113,7 @@ def crear_perfil():
 
     boton_guardar = tk.Button(perfil_window, text="Guardar Perfil", command=guardar_perfil)
     boton_guardar.grid(row=4, columnspan=2, pady=10)
-
+    #interfaz#
 def acceder_sistema():
     acceso_window = tk.Toplevel(root)
     acceso_window.title("Acceder al Sistema")
@@ -126,7 +127,7 @@ def acceder_sistema():
     label_apellido.grid(row=1, column=0, padx=10, pady=5)
     entry_apellido = tk.Entry(acceso_window)
     entry_apellido.grid(row=1, column=1, padx=10, pady=5)
-
+#Acceder Usuario"
     def acceder_usuario():
         nombre = entry_nombre.get()
         apellido = entry_apellido.get()
@@ -148,7 +149,7 @@ def acceder_sistema():
 
     boton_acceder = tk.Button(acceso_window, text="Acceder", command=acceder_usuario)
     boton_acceder.grid(row=2, columnspan=2, pady=10)
-
+#Parte de la interfaz#
 def acceder_sistema_real(usuario):
     bar = Bar()
     bar.mostrar_bebidas(usuario.tipo)
@@ -168,7 +169,7 @@ def acceder_sistema_real(usuario):
         if not bar.palco_disponible:
             print("Lo siento, el palco ya está ocupado.")
             return
-        usuario.crear_reserva(concierto, cantidad)
+        usuarios.crear_reserva(concierto, cantidad)
         bar.palco_disponible = False
         print("Palco reservado.")
     else:
@@ -183,4 +184,17 @@ def opcion_acceder_sistema():
         messagebox.showerror("Error", "No hay usuarios registrados. Por favor, crea un perfil primero.")
     else:
         acceder_sistema()
+def salir_sistema():
+    root.quit()
+def opcion_salir_sistema():
+    salir_sistema()
+etiqueta = tk.Label(root, text="Bienvenido al Sistema de Reservas")
+etiqueta.pack(pady=20)
+boton_crear_perfil = tk.Button(root, text="Crear Perfil", command=opcion_crear_perfil)
+boton_crear_perfil.pack()
+boton_acceder_sistema = tk.Button(root, text="Acceder al Sistema", command=opcion_acceder_sistema)
+boton_acceder_sistema.pack()
+boton_salir_sistema = tk.Button(root, text="Salir", command=opcion_salir_sistema)
+boton_salir_sistema.pack(pady=20)
+#ejecucion principal#
 root.mainloop()
